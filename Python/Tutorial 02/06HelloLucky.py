00cf85d4a07614f7b5e4b930ecade783edb696c4
Danny <danny@erebus.cymru>
2018-10-15 14:09:56 +0100

    Finished all of the programs from this tutorial

--- Python/Tutorial 02/06HelloLucky.py
+++ Python/Tutorial 02/06HelloLucky.py
@@ -1,3 +1,7 @@
 from random import randint
 
-print("Please input your name")
+name = input("Please input your name: ")
+
+rand_number = randint(1,100)
+
+print("Hello " + name + " your lucky number is " + str(rand_number))
