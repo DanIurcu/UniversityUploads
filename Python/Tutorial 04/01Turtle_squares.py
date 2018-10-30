f31d644e9e76897cf8fcbcec5fe7c9b3bf2dca0b
Danny <danny@erebus.cymru>
2018-10-30 06:47:08 +0000

    Added bonus program
    Added the finished version of the bonus program from tutorial 4

--- Python/Tutorial 04/01Turtle_squares.py
+++ Python/Tutorial 04/01Turtle_squares.py
@@ -2,9 +2,6 @@
 
 from turtle import *
 
-
-
-
 def draw_square(length):
     forward(length)
     left(90)
@@ -23,8 +20,6 @@ def draw_square(length):
 for x in range(1, 5):
     length = input("What length would you like the sides of square " + str(x) + " to be?")
     draw_square(int(length))
-
-
 
 exitonclick()
 
