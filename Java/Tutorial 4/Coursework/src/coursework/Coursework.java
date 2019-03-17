ce1d984d083152a68c49410cd54006f24f6d95e1
Danny <danny@erebus.cymru>
2019-03-17 22:11:23 +0000

    Made ammendments to week four
    Made amendments to week four to fix some errors and to complete the extra work associated with that week.

--- Java/Tutorial 4/Coursework/src/coursework/Coursework.java
+++ Java/Tutorial 4/Coursework/src/coursework/Coursework.java
@@ -3,75 +3,202 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package coursework;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
-import javax.swing.JFrame;
-import java.awt.event.KeyListener;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import javax.swing.*;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.util.ArrayList;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JToolBar;
 
 /**
  *
  * @author Danny
  */
 public class Coursework extends JFrame implements ActionListener, KeyListener {
-
+    
     JPanel pnl = new JPanel(new BorderLayout());
     JTextArea txtNewNote = new JTextArea();
     JTextArea txtDisplayNotes = new JTextArea();
     ArrayList<String> note = new ArrayList<>();
-
+    ArrayList<String> course = new ArrayList<>();
     
-public static void main(String[] args) {
+    public static void main(String[] args) {
         // This is required for the coursework.
-        JOptionPane.showMessageDialog(null, "Daniel Roberts");
+        JOptionPane.showMessageDialog(null, "Danny Roberts");
         Coursework prg = new Coursework();
-}
+    }
 
-// Using MVC
-public Coursework() {
-    model();
-    view();
-    controller();
-}
- 
-private void addAllNotes() {
-        String allNotes = "";
-        
-        for(String n: note) {
-            allNotes += n + "\n";
+    // Using MVC
+    public Coursework() {
+        model();
+        view();
+        controller();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if ("NewNote".equals(ae.getActionCommand())){
+            addNote(txtNewNote.getText());
+            txtNewNote.setText("");
+        }
+        if ("Close".equals(ae.getActionCommand())){
+          
+        }
+        if ("Exit".equals(ae.getActionCommand())){
+            System.exit(0);
         }
         
-        txtDisplayNotes.setText(allNotes);
+        if("display".equals(ae.getActionCommand())){
+            showCrs(txtNewNote.getText());
+        }
     }
 
-private void addNote(String text) {
-        note.add(txtNewNote.getText());
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println("keyTyped not coded yet.");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("keyPressed not coded yet.");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println("keyReleased not coded yet.");
+    }
+    
+    private void model() {
+        note.add("Arrays are of fixed length and are inflexible.");
+        note.add("ArraysList can be added to and items can be deleted.");
+        course.add("oop");
+        course.add("math");
+        course.add("DSD");
+    }
+
+    private void view() {
+        Font fnt = new Font("Georgia", Font.PLAIN, 24);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu note = new JMenu();
+
+        note = new JMenu("Note");
+        note.setToolTipText("Note tasks");
+        note.setFont(fnt);
+
+        note.add(makeMenuItem("New", "NewNote", "Create a new note.", fnt));
+        note.addSeparator();
+        note.add(makeMenuItem("Close", "Close", "Clear the current note.", fnt));
+
+        
+        JMenu course = new JMenu();
+        
+        course = new JMenu("Course");
+        course.setFont(fnt);
+        course.add(makeMenuItem("OOP","oop", "oop",fnt));
+        course.addSeparator();
+        course.add(makeMenuItem("math", "math", "math", fnt));
+        
+        menuBar.add(course);
+        menuBar.add(note);
+        menuBar.add(makeMenuItem("Exit", "Exit", "Close this program", fnt));
+                
+        this.setJMenuBar(menuBar);
+
+        JToolBar toolBar = new JToolBar();
+        // Setting up the ButtonBar
+        JButton button = null;
+        button = makeButton("Create", "NewNote",
+                "Create a new note.",
+                "New");
+        toolBar.add(button);
+        button = makeButton("closed door", "Close",
+                "Close this note.",
+                "Close");
+        toolBar.add(button);
+        toolBar.addSeparator();
+        button = makeButton("exit", "Exit",
+                "Exit from this program.",
+                "Exit");
+        toolBar.add(button);
+        button = makeButton("display", "display",
+                "display",
+                "display");
+        toolBar.add(button);
+        
+        add(toolBar, BorderLayout.NORTH);
+        
+        JPanel pnlWest = new JPanel();
+        pnlWest.setLayout(new BoxLayout(pnlWest, BoxLayout.Y_AXIS));
+        pnlWest.setBorder(BorderFactory.createLineBorder(Color.black));
+        
+        txtNewNote.setFont(fnt);
+        pnlWest.add(txtNewNote);
+
+        JButton btnAddNote = new JButton("Add note");
+        btnAddNote.setActionCommand("NewNote");
+        btnAddNote.addActionListener(this);
+        pnlWest.add(btnAddNote);
+        
+        add(pnlWest, BorderLayout.WEST);
+
+        JPanel cen = new JPanel();
+        cen.setLayout(new BoxLayout(cen, BoxLayout.Y_AXIS));
+        cen.setBorder(BorderFactory.createLineBorder(Color.black));
+        txtDisplayNotes.setFont(fnt);
+        cen.add(txtDisplayNotes);
+        
+        add(cen, BorderLayout.CENTER);
+
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setTitle("Coursework - Andy Wicks");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);  // Needed to ensure that the items can be seen.
+    }
+
+
+    private void controller() {
         addAllNotes();
     }
-
-
-protected JMenuItem makeMenuItem(String txt,String actionCommand,String toolTipText,Font fnt) {
-    JMenuItem mnuItem = new JMenuItem();
-    mnuItem.setText(txt);
-    mnuItem.setActionCommand(actionCommand);
-    mnuItem.setToolTipText(toolTipText);
-    mnuItem.setFont(fnt);
-    mnuItem.addActionListener(this);
     
-    return mnuItem;
-}
-
-
-    protected JButton makeButton(
+    protected JMenuItem makeMenuItem(
+            String txt,
+            String actionCommand,
+            String toolTipText,
+            Font fnt){
+        JMenuItem mnuItem = new JMenuItem();
+        mnuItem.setText(txt);
+        mnuItem.setActionCommand(actionCommand);
+        mnuItem.setToolTipText(toolTipText);
+        mnuItem.setFont(fnt);
+        mnuItem.addActionListener(this);
+        
+        return mnuItem;
+    }
+    
+   protected JButton makeButton(
             String imageName,
             String actionCommand,
             String toolTipText,
@@ -96,6 +223,7 @@ protected JMenuItem makeMenuItem(String txt,String actionCommand,String toolTipT
             img = new ImageIcon(imgLocation);
             button.setIcon(img);
         } else {
+
             // image NOT found
             button.setText(altText);
             System.err.println("Resource not found: " + imgLocation);
@@ -104,132 +232,35 @@ protected JMenuItem makeMenuItem(String txt,String actionCommand,String toolTipT
         return button;
     }
 
-
-
-private void view() {
-    Font fnt = new Font("Georgia", Font.PLAIN, 24);
-    setExtendedState(JFrame.MAXIMIZED_BOTH);
-    setTitle("Coursework - Daniel Roberts");
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private void addNote(String text) {
+        note.add(txtNewNote.getText());
+        addAllNotes();
+    }
     
-    JPanel pnlTop = new JPanel();
-    JMenuBar menuBar = new JMenuBar();
+    private void showCrs(String text) {
+        course.add(txtNewNote.getText());
+        showAllCrse();
+    }
     
-    JMenu note = new JMenu();
-    note = new JMenu("Note");
-    note.setToolTipText("Note tasks");
-    note.setFont(fnt);
-    note.add(makeMenuItem("New", "NewNote", "Create a new note.", fnt));
-    note.addSeparator();
-    note.add(makeMenuItem("Close", "Close", "Clear the current note.", fnt));
-    
-    menuBar.add(note);
-    
-    
-    JMenu course = new JMenu();
-    course = new JMenu("Course");
-    course.setToolTipText("Courses I am enrolled on");
-    course.setFont(fnt);
-    course.add(makeMenuItem("Object oriented Programming", "OOP", "OOP Notes", fnt));
-    course.addSeparator();
-    course.add(makeMenuItem("Smart Systems", "Smart Systems", "Smart Systems Notes", fnt));
-    course.addSeparator();
-    course.add(makeMenuItem("Math 1011", "Math", "Math 1011 Notes", fnt));
-    course.addSeparator();
-    course.add(makeMenuItem("Database Systems Development", "DSD", "Database Systems Developments Notes", fnt));
-    menuBar.add(course);
-    
-    menuBar.add(makeMenuItem("Exit", "Exit", "Close this program", fnt));
-
-    this.setJMenuBar(menuBar);
-    
-    JToolBar toolBar = new JToolBar();
-    // Setting up the ButtonBar
-    JButton button = null;
-    button = makeButton("Create", "NewNote",
-                "Create a new note.",
-                "New");
-    toolBar.add(button);
-    button = makeButton("closed door", "Close",
-                "Close this note.",
-                "Close");
-    toolBar.add(button);
-    toolBar.addSeparator();
-    button = makeButton("exit", "Exit",
-                "Exit from this program.",
-                "Exit");
-    toolBar.add(button);
-    add(toolBar, BorderLayout.NORTH);
-    
-    add(pnlTop, BorderLayout.NORTH);
-    pnlTop.add(toolBar, BorderLayout.NORTH);
-    
-    
-    JPanel cen = new JPanel();
-    cen.setLayout(new BoxLayout(cen, BoxLayout.Y_AXIS));
-    cen.setBorder(BorderFactory.createLineBorder(Color.black));
-    txtDisplayNotes.setFont(fnt);
-    cen.add(txtDisplayNotes);
+    private void showAllCrse() {
+        String allCourses = "";
         
-    add(cen, BorderLayout.CENTER);
-
-    
-    JPanel pnlWest = new JPanel();
-    pnlWest.setLayout(new BoxLayout(pnlWest, BoxLayout.Y_AXIS));
-    pnlWest.setBorder(BorderFactory.createLineBorder(Color.black));
+        for(String n: course) {
+            allCourses += n + "\n";
+        }
         
-    txtNewNote.setFont(fnt);
-    pnlWest.add(txtNewNote);
+        txtDisplayNotes.setText(allCourses);
 
-    JButton btnAddNote = new JButton("Add note");
-    btnAddNote.setActionCommand("NewNote");
-    btnAddNote.addActionListener(this);
-    pnlWest.add(btnAddNote);
+    }
+    
+    private void addAllNotes() {
+        String allNotes = "";
         
-    add(pnlWest, BorderLayout.WEST);
-
-    
-    setExtendedState(JFrame.MAXIMIZED_BOTH);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-
-    
-    setVisible(true);  // Needed to ensure that the items can be seen.
-    }
-
-    @Override
-     public void actionPerformed(ActionEvent ae){
-         if ("NewNote".equals(ae.getActionCommand())){
-             addNote(txtNewNote.getText());
-             txtNewNote.setText(""); 
-         }   
-         if ("Exit".equals(ae.getActionCommand())) {
-            System.exit(0);
-            }
-    }
-    
-    @Override
-    public void keyTyped(KeyEvent e) {
-        System.out.println("keyTyped not coded yet.");
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        System.out.println("keyPressed not coded yet.");
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        System.out.println("keyReleased not coded yet.");
-    }
-
-    private void model() {
-        note.add("Arrays are of fixed length and are inflexible.");
-        note.add("ArraysList can be added to and items can be deleted.");
-    }
-
-    private void controller() {
-      addAllNotes();
+        for(String n: note) {
+            allNotes += n + "\n";
+        }
+        
+        txtDisplayNotes.setText(allNotes);
     }
 
 }
